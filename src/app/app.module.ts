b3de77dd456ef15af8bf3fc68df180fbf8d53d91
memory660 <poub660@gmail.com>
2021-12-10 11:22:12 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { CardComponent } from "./components/card/card.component";
 import { ListComponent } from "./components/list/list.component";
-import { DirectiveConditionnalComponent } from './directive-conditionnal/directive-conditionnal.component';
-import { MyDirective1Directive } from './directive-conditionnal/my-directive1.directive';
+import { DirectiveConditionnalComponent } from "./directive-conditionnal/directive-conditionnal.component";
+import { MyDirective1Directive } from "./directive-conditionnal/my-directive1.directive";
+import { MySelectorComponent } from "./custom-dropdown/my-selector/my-selector.component";
+import { Client1Component } from "./custom-dropdown/client1/client1.component";
+import { Client2Component } from "./custom-dropdown/client2/client2.component";
 
 @NgModule({
-  declarations: [AppComponent, ListComponent, CardComponent, DirectiveConditionnalComponent, MyDirective1Directive],
+  declarations: [
+    AppComponent,
+    ListComponent,
+    CardComponent,
+    DirectiveConditionnalComponent,
+    MyDirective1Directive,
+    MySelectorComponent,
+    Client1Component,
+    Client2Component,
+  ],
   imports: [BrowserModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
